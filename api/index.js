ea6c66563852e5f96950a04919c4ffa95baf1d2f
ashutoshsingh2073 <145258929+ashutoshsingh2073@users.noreply.github.com>
2024-07-13 19:30:39 +0530

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/users.route.js';
 dotenv.config();
 
 
@@ -9,9 +10,12 @@ const app = express();
     console.log('Server is running on port 3000');
  });
  
- mongoose.connect(process.env.MONGO).then(() => {
+ mongoose
+ .connect(process.env.MONGO).then(() => {
     console.log('connected to Mongodb');
 })
 .catch((err) =>{
     console.log(err);
-})
+});
+
+app.use("/api/user", userRoutes);
