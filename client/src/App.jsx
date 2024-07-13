88238d520e3ac64cb16c340ef39f5741874035df
ashutoshsingh2073 <145258929+ashutoshsingh2073@users.noreply.github.com>
2024-07-13 10:11:54 +0530

    "Added Header component and imported it into App component"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import About from './pages/About';
 import Profile from './pages/Profile'; 
 import SignUp from './pages/SignUp'; 
 import SignIn from './pages/SignIn';  
+import Header from './components/Header';
+
 export default function App() {
   return (
     <BrowserRouter>
-    {/* {Header} */}
+    <Header/>
     <Routes>
       <Route>
         <Route path="/" element={<Home />}></Route>
